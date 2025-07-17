aeb0b4fa23ebe43163bc265bb786fbde1abfe606
unknown <atima.kae@rmutto.ac.th>
2025-07-17 12:26:36 +0700

    lab02

---
+++ app/routes/mai-lab02.tsx
@@ -0,0 +1,14 @@
+function MyProfile () {
+ return(
+  <div>
+    <p className="text-2xl">Mai Profile</p>
+    <img src="/images/cat.jpg" className="w-1/4" />
+    <p className="text-lg">
+        Name: Atima Kaewprapai<br />
+        Student ID:026730491009-9<br />
+        </p>
+  </div>
+ );
+}
+
+export default MyProfile;
